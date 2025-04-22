618e37deffd304008225d3d4d49d04c3e6fd09db
DanielPerez1822 <daniel29lozada@gmail.com>
2025-04-22 17:09:12 -0500

    feat(HU-1): agregar componente de fecha y hora

---
+++ src/components/select_date.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+      <label for="reservation-datetime">Selecciona fecha y hora:</label>
+      <input
+        type="datetime-local"
+        id="reservation-datetime"
+        v-model="selectedDateTime"
+        @change="emitDateTime"
+      />
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "DateTimePicker",
+    data() {
+      return {
+        selectedDateTime: "",
+      };
+    },
+    methods: {
+      emitDateTime() {
+        this.$emit("update:datetime", this.selectedDateTime);
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  label {
+    font-weight: bold;
+    margin-right: 8px;
+    color: #0056b3;
+  }
+
+  input[type="datetime-local"] {
+    padding: 8px;
+    border: 1px solid #999999;  
+    border-radius: 4px;
+    font-size: 14px;
+    background-color: #f0f0f0;  
+  }
+  </style>
---
+++ src/views/HomePage.vue
@@ -0,0 +1,69 @@
+<template>
+  <BaseLayout>
+    <div id="container">
+      <strong>Bienvenido a la plataforma de Gestión de Reservas.</strong>
+      <div class="buttons">
+        <button @click="goToGestionReservasPage">Ir a Gestión de Reservas</button>
+      </div>
+    </div>
+  </BaseLayout>
+</template>
+
+<script setup lang="ts">
+import BaseLayout from '@/components/BaseLayout.vue';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+const goToGestionReservasPage = () => {
+  router.push('/gestion-reservas').then(() => {
+    router.go(0); // Force the page to refresh dynamically
+  });
+};
+</script>
+
+<style scoped>
+#container {
+  text-align: center;
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 50%;
+  transform: translateY(-50%);
+}
+
+#container strong {
+  font-size: 20px;
+  line-height: 26px;
+}
+
+#container p {
+  font-size: 16px;
+  line-height: 22px;
+  color: #8c8c8c;
+  margin: 0;
+}
+
+.buttons {
+  margin-top: 20px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: 10px;
+}
+
+button {
+  padding: 10px 20px;
+  font-size: 16px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  background-color: #0078D4;
+  color: white;
+  transition: background-color 0.3s;
+}
+
+button:hover {
+  background-color: #005a9e;
+}
+</style>
